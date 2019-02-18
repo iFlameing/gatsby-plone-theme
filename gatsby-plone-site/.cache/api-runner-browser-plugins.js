5f8c8931c9b651e39a0e4e1c841e1709fe45ce36
iFlameing <ialokkumarsingh0@gmail.com>
2019-02-18 20:35:12 +0530

    intial commit

---
+++ gatsby-plone-site/.cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/home/iflameing/Documents/github/gatsby-theme/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/home/iflameing/Documents/github/gatsby-theme/packages/gatsby-plone-theme/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
